30a74fa0884789f3096ccecf600eb09e9fe5dcf0
Antonija Simic <antonija1023@gmail.com>
2022-04-08 15:42:23 +0200

    connect app to heroku api

--- README.md
+++ README.md
@@ -1,2 +1,2 @@
 # todos
-A todo list with which I teach my students to manipulate DOM
+A todo list with which I teach my students to manipulate the DOM
--- main.js
+++ main.js
@@ -1,6 +1,45 @@
+const messages = {
+    ERROR_MESSAGE_EMPTY: {
+        text: "Item can't be empty", 
+        status: "error"
+    },
+    ERROR_MESSAGE_ALREADY_EXISTS: {
+        text: "Item already exists",
+        status: "error"
+    },
+    ERROR_MESSAGE_SMTH_WENT_WRONG: {
+        text: "Something went wrong",
+        status: "error"
+    },
+    SUCCESS_MESSAGE_ITEM_REMOVED: {
+        text: "Item removed",
+        status: "success"
+    },
+    SUCCESS_MESSAGE_ITEM_ADDED: {
+        text: "Item added",
+        status: "success"
+    },
+    EMPTY_TODOS: {
+        text: "You don't have any todos yet! Add new todo :)",
+        status: "success"
+    }
+}
+
+const { 
+    ERROR_MESSAGE_EMPTY,
+    ERROR_MESSAGE_ALREADY_EXISTS, 
+    ERROR_MESSAGE_SMTH_WENT_WRONG, 
+    SUCCESS_MESSAGE_ITEM_REMOVED,
+    SUCCESS_MESSAGE_ITEM_ADDED,
+    EMPTY_TODOS
+} = messages;
+
+const API_URL = "https://kodiraonica-todos.herokuapp.com/api";
+
 const button = document.getElementById("add");
 const input = document.getElementsByTagName("input")[0];
-let itemValues = [];
+let itemValues;
+
 loadTodoItems();
 
 button.addEventListener("click", function(e){
@@ -8,35 +47,55 @@ button.addEventListener("click", function(e){
     addTodoItem(input.value)
 });
 
-function loadTodoItems() {
-    const todos = localStorage.getItem("todos");
-    if (todos) {
-        JSON.parse(todos).forEach((todoValue) => {
-            const button = createRemoveButton(todoValue);
-            createListItem(button, todoValue);
-            removeTodoItem(button, todoValue);
-         });
-         itemValues = JSON.parse(todos);
+async function loadTodoItems() {
+   await fetch(`${API_URL}/todos`)
+    .then((response) => response.json())
+    .then((json) => itemValues = json.slice(0,4))
+
+    if (itemValues.length > 0) {
+        itemValues.forEach((todoValue) => {
+            const button = createRemoveButton(todoValue._id);
+            createListItem(button, todoValue.title);
+            removeTodoItem(button, todoValue._id);
+        });
+    } else {
+        showMessage(EMPTY_TODOS.text, EMPTY_TODOS.status);
     }
 }
 
 function addTodoItem(value) {
-    if (value.trim() == "") {
-        showErrorMessage("Item can't be empty");
+    const isTodoEmpty = value.trim() == "";
+    const button = createRemoveButton(value);
+    const todoAlreadyExists = itemValues.filter((itemValue) => {
+        if (itemValue.title == value) {
+            return true;
+        }
+    });
+
+    if (todoAlreadyExists.length > 0) {
+        showMessage(ERROR_MESSAGE_ALREADY_EXISTS.text, ERROR_MESSAGE_ALREADY_EXISTS.status);
+        return; 
+    } 
+
+    if (isTodoEmpty) {
+        showMessage(ERROR_MESSAGE_EMPTY.text, ERROR_MESSAGE_EMPTY.status);
         return;
     }
 
-    if (!itemValues.includes(value)) {
-        itemValues.push(value);
-    } else {
-        showErrorMessage("Item already exists");
-        return; 
-    }
-
-    localStorage.setItem("todos", JSON.stringify(itemValues));
-    const button = createRemoveButton(value);
+    itemValues.push(value);
+    fetch(`${API_URL}/todo`, {
+        method: 'POST',
+        body: JSON.stringify({
+            title: value
+        }),
+        headers: {
+            'Content-type': 'application/json; charset=UTF-8',
+        },
+    })
+    .then((response) => response.json())
+    .then(() => showMessage(SUCCESS_MESSAGE_ITEM_ADDED.text, SUCCESS_MESSAGE_ITEM_ADDED.status));
+    
     createListItem(button, value);
-    removeTodoItem(button, value);
     resetForm();
 }
 
@@ -47,41 +106,49 @@ function resetForm(){
 
 function createListItem(button, value) {
     const li = document.createElement("li");
+    const span = document.createElement("span");
     const ul = document.getElementsByTagName("ul")[0];
     ul.appendChild(li);
-    li.innerHTML = value;
+    span.innerHTML = value;
+    li.appendChild(span);
     li.appendChild(button);
 }
 
 function createRemoveButton(value){
     const button = document.createElement("button");
     button.innerHTML = "remove";
-    button.setAttribute("id", `removeBtn-${value.trim().replaceAll(" ", "")}`)
+    button.setAttribute("id", `removeBtn-${value}`)
     return button;
 }
 
 function removeTodoItem(button, value) {
-    button.addEventListener("click", function() {
+    button.addEventListener("click", async function() {
         button.parentElement.remove();
-        const newItemValues = itemValues.filter((item) => item !== value);
-        itemValues = newItemValues;
-        localStorage.setItem("todos", JSON.stringify(itemValues));
+        await fetch(`${API_URL}/delete/${value}`, {
+            method: 'DELETE',
+        }).then((res) => {
+            if (res.status == 200) {
+                showMessage(SUCCESS_MESSAGE_ITEM_REMOVED.text, SUCCESS_MESSAGE_ITEM_REMOVED.status)
+            } else {
+                showMessage(ERROR_MESSAGE_SMTH_WENT_WRONG.text, ERROR_MESSAGE_SMTH_WENT_WRONG.status)
+            }
+        });    
     });
 }
 
-function showErrorMessage(message) {
-    const err = document.getElementsByClassName("error");
+function showMessage(message, status) {
+    const err = document.getElementsByClassName(status);
     if (err.length > 0) {
         err[0].innerHTML = message;
         return false;
     }
     const div = document.createElement("div");
     const body = document.getElementsByTagName("body")[0];
-    div.setAttribute("class", "error");
+    div.setAttribute("class", status);
     body.append(div);
     div.innerHTML = message;
 
     setTimeout(() => {
         div.remove();
-    }, 3000)
+    }, 5000)
 }
