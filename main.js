83512a4fd5449a6345a2fb6d433ac2404904f57f
Karlo Strunje <karlostrunje96@gmail.com>
2022-04-12 19:01:33 +0200

    Fetch API

--- main.js
+++ main.js
@@ -1,68 +1,108 @@
+const messages = {
+    ERROR_MESSAGE_EMPTY :{
+        text: "Item can't be empty",
+        status: "error"
+    },
+    ERROR_MESSAGE_ALREDY_EXISTS: {
+        text: "Item already exists",
+        ststus: "error"
+    },
+    SUCCESS_MESSAGE_ITEMS_LOADED: {
+        text: "Item loaded",
+        ststus: "success"
+    },
+    SUCCESS_MESSAGE_ITEM_CREATED : {
+        text: "Item created",
+        status: "success"
+    },
+    SUCCESS_MESSAGE_ITEM_REMOVED: {
+        text: "Item removed",
+        status: "success"
+    },
+    EMPTY_TODOS: {
+        text:"You don't have any todos yet: Add some",
+        status:"success"
+    }
+};
+
+const {
+    ERROR_MESSAGE_EMPTY,
+    ERROR_MESSAGE_ALREDY_EXISTS,
+    SUCCESS_MESSAGE_ITEMS_LOADED,
+    SUCCESS_MESSAGE_ITEM_CREATED,
+    SUCCESS_MESSAGE_ITEM_REMOVED,
+    EMPTY_TODOS
+} = messages;
+
 const button = document.getElementById("add");
 const input = document.getElementsByTagName("input")[0];
 const LOCAL_STORAGE_KEY = "todos";
+const API_URL= "https://kodiraonica-todos.herokuapp.com/api"
 let itemValues = [];
-
 loadTodoItems();
 
-button.addEventListener("click",function(e){
+button.addEventListener("click", function (e) {
     e.preventDefault();
-    addTodoItem(input.value)
+    addTodoItem(input.value);
 });
 
 function loadTodoItems() {
-    const todos = JSON.parse(localStorage.getItem(LOCAL_STORAGE_KEY));
-    
-    if (todos) {
-        todos.forEach(todoItem => {
-           
+    fetch(`${API_URL}/todos`)
+    .then ((response)=>response.json())
+    .then((res)=>itemValues = res)
+    .catch((err)=> console.log(err));
+
+    if (itemValues, length > 0) {
+        itemValues.forEach((todoItem)=>{
             const button = createRemoveButton(todoItem);
-            createListItem(button, todoItem)
-            removeTodoItem(button, todoItem);
-            showMessage("Item loaded", "success")
-
-        }); 
-        itemValues = todos;
+            createListItem(button, todoItem);
+            removeTodoItem(button, todoItem); 
+            showMessage (
+                SUCCESS_MESSAGE_ITEMS_LOADED.text,
+                SUCCESS_MESSAGE_ITEMS_LOADED.status 
+            );   
+        });
+    } else {
+        showMessage(EMPTY_TODOS.text, EMPTY_TODOS.status);
     }
-
-    }
-
+}
     
-
 function addTodoItem(value) {
-    if (value.trim() == ""){
-        showMessage("Item can't be empty", "error");
+    if (value.trim() == "") {
+        showMessage(ERROR_MESSAGE_EMPTY.text, ERROR_MESSAGE_EMPTY.status);
         return;
     }
 
     if (!itemValues.includes(value)) {
-        itemValues.push(value)
+        itemValues.push(value);
     } else {
-        showMessage("Item already exists", "error");
-        return; 
+        showMessage(ERROR_MESSAGE_ALREDY_EXISTS.text, ERROR_MESSAGE_ALREDY_EXISTS.status);
+        return;
     }
 
-    localStorage.setItem(LOCAL_STORAGE_KEY,JSON.stringify(itemValues));
+    localStorage.setItem(LOCAL_STORAGE_KEY, JSON.stringify(itemValues));
+    showMessage(SUCCESS_MESSAGE_ITEM_CREATED.text, SUCCESS_MESSAGE_ITEM_CREATED.status);
     const button = createRemoveButton(value);
-    removeTodoItem(button,value);
-    createListItem(button,value);
-    showMessage("Item aded", "success")
+    createListItem(button, value);
+    removeTodoItem(button, value);
     resetForm();
-} 
+}
 
 function resetForm() {
     const form = document.getElementsByTagName("form")[0];
     form.reset();
 }
 
-function createRemoveButton (value) {
+function createRemoveButton(value) {
     const button = document.createElement("button");
     button.innerHTML = "remove";
-    button.setAttribute("id", `removeBtn-${value.trim().replaceAll(" ", "")}`);
-    return button;
+    button.setAttribute(
+        "id", 
+        `removeBtn-${value.trim().replaceAll(" ", "")}`);
+        return button;
 }
 
-function createListItem (button, value){
+function createListItem(button, value) {
     const ul = document.getElementsByTagName("ul")[0];
     const li = document.createElement("li");
     ul.appendChild(li);
@@ -70,14 +110,14 @@ function createListItem (button, value){
     li.appendChild(button);
 }
 
-function removeTodoItem(button,value)  {
-        button.addEventListener("click",function(){
-            button.parentElement.remove();
-            const newItemValues = itemValues.filter((itemValue) => itemValue !== value);
-            itemValues = newItemValues;
-            localStorage.setItem(LOCAL_STORAGE_KEY, JSON.stringify(itemValues));
-            showMessage("Item removed", "success")
-        });
+function removeTodoItem(button, value) {
+    button.addEventListener("click", function() {
+        button.parentElement.remove();
+        const newItemValues = itemValues.filter((itemValue) => itemValue !== value);
+        itemValues = newItemValues;
+        showMessage(SUCCESS_MESSAGE_ITEM_REMOVED.text, SUCCESS_MESSAGE_ITEM_REMOVED.status);
+        localStorage.setItem(LOCAL_STORAGE_KEY, JSON.stringify(newItemValues));
+    });
 }
 
 function showMessage(message, status) {
@@ -86,13 +126,14 @@ function showMessage(message, status) {
         err[0].innerHTML = message;
         return false;
     }
+
     const div = document.createElement("div");
     const body = document.getElementsByTagName("body")[0];
-    div.setAttribute("class",status);
+    div.setAttribute("class", status);
     body.append(div);
     div.innerHTML = message;
 
     setTimeout(() => {
         div.remove();
-    }, 3000)
+    }, 3000);
 }
